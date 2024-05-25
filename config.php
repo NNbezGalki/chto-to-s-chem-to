05221b428164cda612a4a77b67b4a5b29040ce79
NNbezGalki <137414368+NNbezGalki@users.noreply.github.com>
2024-05-25 23:42:59 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Данные приложения ВКонтакте
-define('VK_APP_ID', 'ВАШ_ID_ПРИЛОЖЕНИЯ'); 
-define('VK_APP_SECRET', 'ВАШ_ЗАЩИЩЕННЫЙ_КЛЮЧ');
-define('VK_REDIRECT_URI', 'https://example.com/vk_callback.php'); 
+define('VK_APP_ID', '51930496'); 
+define('VK_APP_SECRET', 'byx0v8gVXuzf5cZ3f82G');
+define('VK_REDIRECT_URI', 'https://nnbezgalki.github.io/chto-to-s-chem-to/vk_callback.php'); 
 ?>
